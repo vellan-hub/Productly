dfe6a535e70c189dd95401c7055c5fa9d8225768
Vitaly Yakubovski <falkon777.13@gmail.com>
2021-07-19 23:21:57 +0300

    add scripts

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = (env, options) => {
+    const isProduction = options.mode === 'production';
+
+    const config = {
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? false : 'source-map',
+        watch: !isProduction,
+        entry: ['./src/index.js', './src/sass/style.scss'],
+        output: {
+            path: path.join(__dirname, '/dist'),
+            filename: 'script.js',
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: ['@babel/preset-env']
+                      }
+                    }
+                }, {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                    ]
+                },
+            ]
+        },
+
+        plugins: [
+            new CleanWebpackPlugin(),
+            new MiniCssExtractPlugin({
+                filename: 'style.css'
+            })
+        ]
+    }
+
+    return config;
+}
